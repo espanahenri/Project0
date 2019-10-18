fbe924a8fadc4b09cef04fdaf0cd9846138585b2
Henri Espana <henri.espana14@live.com>
2019-10-18 17:20:30 -0400

    Created Checking Account Class

---
+++ Project0/CheckingAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0
+{
+    class CheckingAccount : IAccount
+    {
+        public decimal InterestRate { get; set; }
+        public decimal Funds { get; set; }
+        public int AccountNumber { get; set; }
+        public void Deposit(decimal amt)
+        { 
+            
+        }
+        public void Withdraw(decimal amt)
+        { 
+        }
+    }
+}
