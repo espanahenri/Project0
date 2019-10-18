476319ad0baf0773fc666984cd393acb65ade69e
Henri Espana <henri.espana14@live.com>
2019-10-18 15:44:00 -0400

    Created a account interface

---
+++ Project0/IAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0
+{
+    interface IAccount
+    {
+        decimal InterestRate { get; set; }
+        decimal Funds { get; set; }
+        int AccountNumber { get; set; }
+        void Deposit(decimal amt);
+        void Withdrawl(decimal amt);
+
+    }
+}
